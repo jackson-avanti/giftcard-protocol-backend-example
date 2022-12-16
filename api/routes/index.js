e7b2dbb6ac5e844a2a415272ca064ba67a4e3a5b
jacksonvfo <jackson.oliveira@penseavanti.com.br>
2022-12-16 13:54:10 -0300

    chore: added routes to vercel

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('api/1', function(req, res, next) {
+router.get('/1', function(req, res, next) {
   res.status(200).json(
     {
       balance: 100000,
